a0aec6308f095bd5b55234530c5d5ce869d7bc47
jmpark1115@gmail.com <gitFaith20178!>
2019-06-08 21:57:16 +0900

    학습을 위해 예제 삭제

--- bithumb_trading.py
+++ bithumb_trading.py
@@ -1,4 +1,4 @@
-# 이 프로그램은 지갑에 있는 밸런스를 체크하는 프로그램이다.
+# 이 프로그램은 빗썸 api 를 테스트하는 프로그램이다.
 
 from bithumb import Bithumb
 from configparser import ConfigParser, NoSectionError
@@ -16,55 +16,5 @@ baseCurrency   = 'KRW'
 
 # object created
 bithumb = Bithumb(bithumbKey, bithumbSecret)
-
-#balance check
-print("=== check balance ===")
-walletinfo = bithumb.balance(targetCurrency)
-print(walletinfo)
-
-#check orderbook
-marketinfo = bithumb.orderbook(targetCurrency)
-print(marketinfo)
-askprice = float(marketinfo["data"]["asks"][0]["price"])
-bidprice = float(marketinfo["data"]["bids"][0]["price"])
-askqty = float(marketinfo["data"]["asks"][0]["quantity"])
-bidqty = float(marketinfo["data"]["bids"][0]["quantity"])
-print("askprice[%d] bidprice[%d] askqty[%d] bidqty[%d]"
-      %(askprice, bidprice, askqty, bidqty))
-
-
-#buy
-units = 10
-price = askprice - 100
-# price = askprice
-response = bithumb.place(targetCurrency, baseCurrency, units, price, "bid")
-print(response)
-orderNumber = response['order_id']
-print(orderNumber)
-
-#time delay
-time.sleep(5)
-
-
-response = bithumb.order_detail(orderNumber, 'bid', targetCurrency)
-print(response)
-
-response = bithumb.cancel('bid', orderNumber, targetCurrency)
-print(response)
-
-
-dest_address_xrp_coinone = your_address
-dest_tag = 0
-response = bithumb.btc_withdrawal(targetCurrency, dest_address_xrp_coinone, units, dest_tag)
-print(response)
-
-
-bankaccount = your_account
-drawal_price = 1000
-response = bithumb.krw_deposit()
-response = bithumb.krw_withdrawal("090", bankaccount, drawal_price)
-print(response)
-
-
 
 
