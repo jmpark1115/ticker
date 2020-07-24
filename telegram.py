ff4d8ad93f046423cc35ab04fcefc48d5dbd5c60
contact9853 <contact9853@gmail.com>
2020-07-24 22:51:09 +0900

    code clean

--- telegram.py
+++ telegram.py
@@ -1,79 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 import requests
-import json
 
-class Telegram:
-    tg_url_bot_general = "https://api.telegram.org/bot"
+API_URL = "https://api.telegram.org/bot"
 
-    def http_get(self, url):
-        res = requests.get(url, proxies=self.proxies)
-        answer = res.text
-        # answer_json = json.loads(answer.decode('utf8'))
-        answer_json = json.loads(answer)
-        return answer_json
+class Telegram:    
 
-    def __init__(self, key, chat_id) :
-        self.debug = False
+    def __init__(self, key) :
         self.key = key
-        self.to  = chat_id
-        self.proxies = {}
-        self.type = "private"  # 'private' for private chats or 'group' for group chats
-        self.markdown = False
-        self.html = False
-        self.disable_web_page_preview = False
-        self.disable_notification = False
-        self.reply_to_message_id = 0
-        self.tmp_uids = None
 
     def get_me(self):
-        url = self.tg_url_bot_general + self.key + "/getMe"
-        me = self.http_get(url)
-        return me
+        url = API_URL + self.key + "/getMe"
+        resp = requests.get(url)
+        return resp.json()
 
     def get_updates(self):
-        url = self.tg_url_bot_general + self.key + "/getUpdates"
-        if self.debug:
-            print(url)
-        updates = self.http_get(url)
-        if self.debug:
-            print("Content of /getUpdates:")
-            print(updates)
-        if not updates["ok"]:
-            print(updates)
-            return updates
-        else:
-            return updates
+        url = API_URL + self.key + "/getUpdates"
+        resp = requests.get(url)
+        return resp.json()
 
-    def message(self, message):
-        url = self.tg_url_bot_general + self.key + "/sendMessage"
-        # message = "\n".join(message)
-        params = {"chat_id": self.to, "text": message, "disable_web_page_preview": self.disable_web_page_preview,
-                  "disable_notification": self.disable_notification}
-        if self.reply_to_message_id:
-            params["reply_to_message_id"] = self.reply_to_message_id
-        if self.markdown or self.html:
-            parse_mode = "HTML"
-            if self.markdown:
-                parse_mode = "Markdown"
-            params["parse_mode"] = parse_mode
-        if self.debug:
-            print("Trying to /sendMessage:")
-            print(url)
-            print("post params: " + str(params))
-        res = requests.post(url, params=params, proxies=self.proxies)
-        answer = res.text
-        # answer_json = json.loads(answer.decode('utf8'))
-        answer_json = json.loads(answer)
-        if not answer_json["ok"]:
-            print(answer_json)
-            return answer_json
-        else:
-            return answer_json
+    def message(self, message, chat_id):
+        url = API_URL + self.key + "/sendMessage"
+        params = {
+                  "chat_id": chat_id,
+                  "text": message
+        }
+        resp = requests.post(url, params=params)
+        content = resp.json()
+        return content
 
 if __name__ == "__main__":
     # Load Config File
-
     from configparser import ConfigParser, NoSectionError
     # Load Config File
     config = ConfigParser()
@@ -81,5 +38,6 @@ if __name__ == "__main__":
     chat_id    = config.get('ChatBot', 'chatId')
     chat_token = config.get('ChatBot', 'chatToken')
 
-    tg = Telegram(chat_token, chat_id)
-    result = tg.message("welcome to my telegram message service")
+    tg = Telegram(chat_token)
+    result = tg.message("welcome to my telegram message service", chat_id)
+    print(result)
